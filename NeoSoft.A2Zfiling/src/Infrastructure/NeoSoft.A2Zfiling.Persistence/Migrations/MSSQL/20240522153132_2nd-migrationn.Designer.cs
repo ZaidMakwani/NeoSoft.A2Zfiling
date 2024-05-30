202faf1aa096564c595dd840254ced4776b6f3a4
Mayur Kalsekar <mayur.kalsekar@neosoftmail.com>
2024-05-30 16:00:33 +0530

    REmoved Unused files

--- NeoSoft.A2Zfiling/src/Infrastructure/NeoSoft.A2Zfiling.Persistence/Migrations/MSSQL/20240522153132_2nd-migrationn.Designer.cs
+++ NeoSoft.A2Zfiling/src/Infrastructure/NeoSoft.A2Zfiling.Persistence/Migrations/MSSQL/20240522153132_2nd-migrationn.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using NeoSoft.A2Zfiling.Persistence;
 
@@ -11,9 +12,10 @@ using NeoSoft.A2Zfiling.Persistence;
 namespace NeoSoft.A2Zfiling.Persistence.Migrations.MSSQL
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240522153132_2nd-migrationn")]
+    partial class _2ndmigrationn
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -75,42 +77,6 @@ namespace NeoSoft.A2Zfiling.Persistence.Migrations.MSSQL
                         });
                 });
 
-            modelBuilder.Entity("NeoSoft.A2Zfiling.Domain.Entities.Company", b =>
-                {
-                    b.Property<int>("CompanyId")
-                        .ValueGeneratedOnAdd()
-                        .HasColumnType("int");
-
-                    SqlServerPropertyBuilderExtensions.UseIdentityColumn(b.Property<int>("CompanyId"), 1L, 1);
-
-                    b.Property<string>("CompanyName")
-                        .IsRequired()
-                        .HasColumnType("nvarchar(max)");
-
-                    b.Property<string>("CreatedBy")
-                        .HasColumnType("nvarchar(max)");
-
-                    b.Property<DateTime>("CreatedDate")
-                        .HasColumnType("datetime2");
-
-                    b.Property<bool>("IsActive")
-                        .HasColumnType("bit");
-
-                    b.Property<string>("LastModifiedBy")
-                        .HasColumnType("nvarchar(max)");
-
-                    b.Property<DateTime?>("LastModifiedDate")
-                        .HasColumnType("datetime2");
-
-                    b.Property<string>("ShortName")
-                        .IsRequired()
-                        .HasColumnType("nvarchar(max)");
-
-                    b.HasKey("CompanyId");
-
-                    b.ToTable("Companies");
-                });
-
             modelBuilder.Entity("NeoSoft.A2Zfiling.Domain.Entities.Event", b =>
                 {
                     b.Property<Guid>("EventId")
@@ -167,7 +133,7 @@ namespace NeoSoft.A2Zfiling.Persistence.Migrations.MSSQL
                             Artist = "John Egbert",
                             CategoryId = new Guid("b0788d2f-8003-43c1-92a4-edc76a7c5dde"),
                             CreatedDate = new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified),
-                            Date = new DateTime(2024, 11, 23, 9, 54, 18, 600, DateTimeKind.Utc).AddTicks(9389),
+                            Date = new DateTime(2024, 11, 22, 15, 31, 31, 707, DateTimeKind.Utc).AddTicks(2733),
                             Description = "Join John for his farwell tour across 15 continents. John really needs no introduction since he has already mesmerized the world with his banjo.",
                             ImageUrl = "https://gillcleerenpluralsight.blob.core.windows.net/files/GloboTicket/banjo.jpg",
                             Name = "John Egbert Live",
@@ -179,7 +145,7 @@ namespace NeoSoft.A2Zfiling.Persistence.Migrations.MSSQL
                             Artist = "Michael Johnson",
                             CategoryId = new Guid("b0788d2f-8003-43c1-92a4-edc76a7c5dde"),
                             CreatedDate = new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified),
-                            Date = new DateTime(2025, 2, 23, 9, 54, 18, 600, DateTimeKind.Utc).AddTicks(9414),
+                            Date = new DateTime(2025, 2, 22, 15, 31, 31, 707, DateTimeKind.Utc).AddTicks(2906),
                             Description = "Michael Johnson doesn't need an introduction. His 25 concert across the globe last year were seen by thousands. Can we add you to the list?",
                             ImageUrl = "https://gillcleerenpluralsight.blob.core.windows.net/files/GloboTicket/michael.jpg",
                             Name = "The State of Affairs: Michael Live!",
@@ -191,7 +157,7 @@ namespace NeoSoft.A2Zfiling.Persistence.Migrations.MSSQL
                             Artist = "DJ 'The Mike'",
                             CategoryId = new Guid("b0788d2f-8003-43c1-92a4-edc76a7c5dde"),
                             CreatedDate = new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified),
-                            Date = new DateTime(2024, 9, 23, 9, 54, 18, 600, DateTimeKind.Utc).AddTicks(9427),
+                            Date = new DateTime(2024, 9, 22, 15, 31, 31, 707, DateTimeKind.Utc).AddTicks(2935),
                             Description = "DJs from all over the world will compete in this epic battle for eternal fame.",
                             ImageUrl = "https://gillcleerenpluralsight.blob.core.windows.net/files/GloboTicket/dj.jpg",
                             Name = "Clash of the DJs",
@@ -203,7 +169,7 @@ namespace NeoSoft.A2Zfiling.Persistence.Migrations.MSSQL
                             Artist = "Manuel Santinonisi",
                             CategoryId = new Guid("b0788d2f-8003-43c1-92a4-edc76a7c5dde"),
                             CreatedDate = new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified),
-                            Date = new DateTime(2024, 9, 23, 9, 54, 18, 600, DateTimeKind.Utc).AddTicks(9438),
+                            Date = new DateTime(2024, 9, 22, 15, 31, 31, 707, DateTimeKind.Utc).AddTicks(2957),
                             Description = "Get on the hype of Spanish Guitar concerts with Manuel.",
                             ImageUrl = "https://gillcleerenpluralsight.blob.core.windows.net/files/GloboTicket/guitar.jpg",
                             Name = "Spanish guitar hits with Manuel",
@@ -215,7 +181,7 @@ namespace NeoSoft.A2Zfiling.Persistence.Migrations.MSSQL
                             Artist = "Many",
                             CategoryId = new Guid("fe98f549-e790-4e9f-aa16-18c2292a2ee9"),
                             CreatedDate = new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified),
-                            Date = new DateTime(2025, 3, 23, 9, 54, 18, 600, DateTimeKind.Utc).AddTicks(9450),
+                            Date = new DateTime(2025, 3, 22, 15, 31, 31, 707, DateTimeKind.Utc).AddTicks(2977),
                             Description = "The best tech conference in the world",
                             ImageUrl = "https://gillcleerenpluralsight.blob.core.windows.net/files/GloboTicket/conf.jpg",
                             Name = "Techorama 2021",
@@ -227,7 +193,7 @@ namespace NeoSoft.A2Zfiling.Persistence.Migrations.MSSQL
                             Artist = "Nick Sailor",
                             CategoryId = new Guid("6313179f-7837-473a-a4d5-a5571b43e6a6"),
                             CreatedDate = new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified),
-                            Date = new DateTime(2025, 1, 23, 9, 54, 18, 600, DateTimeKind.Utc).AddTicks(9463),
+                            Date = new DateTime(2025, 1, 22, 15, 31, 31, 707, DateTimeKind.Utc).AddTicks(3007),
                             Description = "The critics are over the moon and so will you after you've watched this sing and dance extravaganza written by Nick Sailor, the man from 'My dad and sister'.",
                             ImageUrl = "https://gillcleerenpluralsight.blob.core.windows.net/files/GloboTicket/musical.jpg",
                             Name = "To the Moon and Back",
@@ -268,7 +234,7 @@ namespace NeoSoft.A2Zfiling.Persistence.Migrations.MSSQL
 
                     b.HasKey("IndustryId");
 
-                    b.ToTable("Industries");
+                    b.ToTable("Industry");
                 });
 
             modelBuilder.Entity("NeoSoft.A2Zfiling.Domain.Entities.Message", b =>
@@ -365,7 +331,7 @@ namespace NeoSoft.A2Zfiling.Persistence.Migrations.MSSQL
                             Id = new Guid("7e94bc5b-71a5-4c8c-bc3b-71bb7976237e"),
                             CreatedDate = new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified),
                             OrderPaid = true,
-                            OrderPlaced = new DateTime(2024, 5, 23, 9, 54, 18, 600, DateTimeKind.Utc).AddTicks(9479),
+                            OrderPlaced = new DateTime(2024, 5, 22, 15, 31, 31, 707, DateTimeKind.Utc).AddTicks(3037),
                             OrderTotal = 400,
                             UserId = new Guid("a441eb40-9636-4ee6-be49-a66c5ec1330b")
                         },
@@ -374,7 +340,7 @@ namespace NeoSoft.A2Zfiling.Persistence.Migrations.MSSQL
                             Id = new Guid("86d3a045-b42d-4854-8150-d6a374948b6e"),
                             CreatedDate = new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified),
                             OrderPaid = true,
-                            OrderPlaced = new DateTime(2024, 5, 23, 9, 54, 18, 600, DateTimeKind.Utc).AddTicks(9496),
+                            OrderPlaced = new DateTime(2024, 5, 22, 15, 31, 31, 707, DateTimeKind.Utc).AddTicks(3066),
                             OrderTotal = 135,
                             UserId = new Guid("ac3cfaf5-34fd-4e4d-bc04-ad1083ddc340")
                         },
@@ -383,7 +349,7 @@ namespace NeoSoft.A2Zfiling.Persistence.Migrations.MSSQL
                             Id = new Guid("771cca4b-066c-4ac7-b3df-4d12837fe7e0"),
                             CreatedDate = new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified),
                             OrderPaid = true,
-                            OrderPlaced = new DateTime(2024, 5, 23, 9, 54, 18, 600, DateTimeKind.Utc).AddTicks(9507),
+                            OrderPlaced = new DateTime(2024, 5, 22, 15, 31, 31, 707, DateTimeKind.Utc).AddTicks(3092),
                             OrderTotal = 85,
                             UserId = new Guid("d97a15fc-0d32-41c6-9ddf-62f0735c4c1c")
                         },
@@ -392,7 +358,7 @@ namespace NeoSoft.A2Zfiling.Persistence.Migrations.MSSQL
                             Id = new Guid("3dcb3ea0-80b1-4781-b5c0-4d85c41e55a6"),
                             CreatedDate = new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified),
                             OrderPaid = true,
-                            OrderPlaced = new DateTime(2024, 5, 23, 9, 54, 18, 600, DateTimeKind.Utc).AddTicks(9518),
+                            OrderPlaced = new DateTime(2024, 5, 22, 15, 31, 31, 707, DateTimeKind.Utc).AddTicks(3115),
                             OrderTotal = 245,
                             UserId = new Guid("4ad901be-f447-46dd-bcf7-dbe401afa203")
                         },
@@ -401,7 +367,7 @@ namespace NeoSoft.A2Zfiling.Persistence.Migrations.MSSQL
                             Id = new Guid("e6a2679c-79a3-4ef1-a478-6f4c91b405b6"),
                             CreatedDate = new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified),
                             OrderPaid = true,
-                            OrderPlaced = new DateTime(2024, 5, 23, 9, 54, 18, 600, DateTimeKind.Utc).AddTicks(9587),
+                            OrderPlaced = new DateTime(2024, 5, 22, 15, 31, 31, 707, DateTimeKind.Utc).AddTicks(3137),
                             OrderTotal = 142,
                             UserId = new Guid("7aeb2c01-fe8e-4b84-a5ba-330bdf950f5c")
                         },
@@ -410,7 +376,7 @@ namespace NeoSoft.A2Zfiling.Persistence.Migrations.MSSQL
                             Id = new Guid("f5a6a3a0-4227-4973-abb5-a63fbe725923"),
                             CreatedDate = new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified),
                             OrderPaid = true,
-                            OrderPlaced = new DateTime(2024, 5, 23, 9, 54, 18, 600, DateTimeKind.Utc).AddTicks(9602),
+                            OrderPlaced = new DateTime(2024, 5, 22, 15, 31, 31, 707, DateTimeKind.Utc).AddTicks(3163),
                             OrderTotal = 40,
                             UserId = new Guid("f5a6a3a0-4227-4973-abb5-a63fbe725923")
                         },
@@ -419,7 +385,7 @@ namespace NeoSoft.A2Zfiling.Persistence.Migrations.MSSQL
                             Id = new Guid("ba0eb0ef-b69b-46fd-b8e2-41b4178ae7cb"),
                             CreatedDate = new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified),
                             OrderPaid = true,
-                            OrderPlaced = new DateTime(2024, 5, 23, 9, 54, 18, 600, DateTimeKind.Utc).AddTicks(9613),
+                            OrderPlaced = new DateTime(2024, 5, 22, 15, 31, 31, 707, DateTimeKind.Utc).AddTicks(3185),
                             OrderTotal = 116,
                             UserId = new Guid("7aeb2c01-fe8e-4b84-a5ba-330bdf950f5c")
                         });
---
+++ NeoSoft.A2Zfiling/src/Infrastructure/NeoSoft.A2Zfiling.Persistence/Migrations/MSSQL/20240522153132_2nd-migrationn.cs
@@ -0,0 +1,220 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NeoSoft.A2Zfiling.Persistence.Migrations.MSSQL
+{
+    public partial class _2ndmigrationn : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Industry",
+                columns: table => new
+                {
+                    IndustryId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    IndustryName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ShortName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    IsActive = table.Column<bool>(type: "bit", nullable: false),
+                    CreatedBy = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CreatedDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    LastModifiedBy = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    LastModifiedDate = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Industry", x => x.IndustryId);
+                });
+
+            migrationBuilder.UpdateData(
+                table: "Events",
+                keyColumn: "EventId",
+                keyValue: new Guid("1babd057-e980-4cb3-9cd2-7fdd9e525668"),
+                column: "Date",
+                value: new DateTime(2025, 3, 22, 15, 31, 31, 707, DateTimeKind.Utc).AddTicks(2977));
+
+            migrationBuilder.UpdateData(
+                table: "Events",
+                keyColumn: "EventId",
+                keyValue: new Guid("3448d5a4-0f72-4dd7-bf15-c14a46b26c00"),
+                column: "Date",
+                value: new DateTime(2025, 2, 22, 15, 31, 31, 707, DateTimeKind.Utc).AddTicks(2906));
+
+            migrationBuilder.UpdateData(
+                table: "Events",
+                keyColumn: "EventId",
+                keyValue: new Guid("62787623-4c52-43fe-b0c9-b7044fb5929b"),
+                column: "Date",
+                value: new DateTime(2024, 9, 22, 15, 31, 31, 707, DateTimeKind.Utc).AddTicks(2957));
+
+            migrationBuilder.UpdateData(
+                table: "Events",
+                keyColumn: "EventId",
+                keyValue: new Guid("adc42c09-08c1-4d2c-9f96-2d15bb1af299"),
+                column: "Date",
+                value: new DateTime(2025, 1, 22, 15, 31, 31, 707, DateTimeKind.Utc).AddTicks(3007));
+
+            migrationBuilder.UpdateData(
+                table: "Events",
+                keyColumn: "EventId",
+                keyValue: new Guid("b419a7ca-3321-4f38-be8e-4d7b6a529319"),
+                column: "Date",
+                value: new DateTime(2024, 9, 22, 15, 31, 31, 707, DateTimeKind.Utc).AddTicks(2935));
+
+            migrationBuilder.UpdateData(
+                table: "Events",
+                keyColumn: "EventId",
+                keyValue: new Guid("ee272f8b-6096-4cb6-8625-bb4bb2d89e8b"),
+                column: "Date",
+                value: new DateTime(2024, 11, 22, 15, 31, 31, 707, DateTimeKind.Utc).AddTicks(2733));
+
+            migrationBuilder.UpdateData(
+                table: "Orders",
+                keyColumn: "OrderId",
+                keyValue: new Guid("3dcb3ea0-80b1-4781-b5c0-4d85c41e55a6"),
+                column: "OrderPlaced",
+                value: new DateTime(2024, 5, 22, 15, 31, 31, 707, DateTimeKind.Utc).AddTicks(3115));
+
+            migrationBuilder.UpdateData(
+                table: "Orders",
+                keyColumn: "OrderId",
+                keyValue: new Guid("771cca4b-066c-4ac7-b3df-4d12837fe7e0"),
+                column: "OrderPlaced",
+                value: new DateTime(2024, 5, 22, 15, 31, 31, 707, DateTimeKind.Utc).AddTicks(3092));
+
+            migrationBuilder.UpdateData(
+                table: "Orders",
+                keyColumn: "OrderId",
+                keyValue: new Guid("7e94bc5b-71a5-4c8c-bc3b-71bb7976237e"),
+                column: "OrderPlaced",
+                value: new DateTime(2024, 5, 22, 15, 31, 31, 707, DateTimeKind.Utc).AddTicks(3037));
+
+            migrationBuilder.UpdateData(
+                table: "Orders",
+                keyColumn: "OrderId",
+                keyValue: new Guid("86d3a045-b42d-4854-8150-d6a374948b6e"),
+                column: "OrderPlaced",
+                value: new DateTime(2024, 5, 22, 15, 31, 31, 707, DateTimeKind.Utc).AddTicks(3066));
+
+            migrationBuilder.UpdateData(
+                table: "Orders",
+                keyColumn: "OrderId",
+                keyValue: new Guid("ba0eb0ef-b69b-46fd-b8e2-41b4178ae7cb"),
+                column: "OrderPlaced",
+                value: new DateTime(2024, 5, 22, 15, 31, 31, 707, DateTimeKind.Utc).AddTicks(3185));
+
+            migrationBuilder.UpdateData(
+                table: "Orders",
+                keyColumn: "OrderId",
+                keyValue: new Guid("e6a2679c-79a3-4ef1-a478-6f4c91b405b6"),
+                column: "OrderPlaced",
+                value: new DateTime(2024, 5, 22, 15, 31, 31, 707, DateTimeKind.Utc).AddTicks(3137));
+
+            migrationBuilder.UpdateData(
+                table: "Orders",
+                keyColumn: "OrderId",
+                keyValue: new Guid("f5a6a3a0-4227-4973-abb5-a63fbe725923"),
+                column: "OrderPlaced",
+                value: new DateTime(2024, 5, 22, 15, 31, 31, 707, DateTimeKind.Utc).AddTicks(3163));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Industry");
+
+            migrationBuilder.UpdateData(
+                table: "Events",
+                keyColumn: "EventId",
+                keyValue: new Guid("1babd057-e980-4cb3-9cd2-7fdd9e525668"),
+                column: "Date",
+                value: new DateTime(2025, 3, 22, 14, 50, 40, 369, DateTimeKind.Utc).AddTicks(148));
+
+            migrationBuilder.UpdateData(
+                table: "Events",
+                keyColumn: "EventId",
+                keyValue: new Guid("3448d5a4-0f72-4dd7-bf15-c14a46b26c00"),
+                column: "Date",
+                value: new DateTime(2025, 2, 22, 14, 50, 40, 369, DateTimeKind.Utc).AddTicks(107));
+
+            migrationBuilder.UpdateData(
+                table: "Events",
+                keyColumn: "EventId",
+                keyValue: new Guid("62787623-4c52-43fe-b0c9-b7044fb5929b"),
+                column: "Date",
+                value: new DateTime(2024, 9, 22, 14, 50, 40, 369, DateTimeKind.Utc).AddTicks(137));
+
+            migrationBuilder.UpdateData(
+                table: "Events",
+                keyColumn: "EventId",
+                keyValue: new Guid("adc42c09-08c1-4d2c-9f96-2d15bb1af299"),
+                column: "Date",
+                value: new DateTime(2025, 1, 22, 14, 50, 40, 369, DateTimeKind.Utc).AddTicks(160));
+
+            migrationBuilder.UpdateData(
+                table: "Events",
+                keyColumn: "EventId",
+                keyValue: new Guid("b419a7ca-3321-4f38-be8e-4d7b6a529319"),
+                column: "Date",
+                value: new DateTime(2024, 9, 22, 14, 50, 40, 369, DateTimeKind.Utc).AddTicks(119));
+
+            migrationBuilder.UpdateData(
+                table: "Events",
+                keyColumn: "EventId",
+                keyValue: new Guid("ee272f8b-6096-4cb6-8625-bb4bb2d89e8b"),
+                column: "Date",
+                value: new DateTime(2024, 11, 22, 14, 50, 40, 369, DateTimeKind.Utc).AddTicks(83));
+
+            migrationBuilder.UpdateData(
+                table: "Orders",
+                keyColumn: "OrderId",
+                keyValue: new Guid("3dcb3ea0-80b1-4781-b5c0-4d85c41e55a6"),
+                column: "OrderPlaced",
+                value: new DateTime(2024, 5, 22, 14, 50, 40, 369, DateTimeKind.Utc).AddTicks(206));
+
+            migrationBuilder.UpdateData(
+                table: "Orders",
+                keyColumn: "OrderId",
+                keyValue: new Guid("771cca4b-066c-4ac7-b3df-4d12837fe7e0"),
+                column: "OrderPlaced",
+                value: new DateTime(2024, 5, 22, 14, 50, 40, 369, DateTimeKind.Utc).AddTicks(196));
+
+            migrationBuilder.UpdateData(
+                table: "Orders",
+                keyColumn: "OrderId",
+                keyValue: new Guid("7e94bc5b-71a5-4c8c-bc3b-71bb7976237e"),
+                column: "OrderPlaced",
+                value: new DateTime(2024, 5, 22, 14, 50, 40, 369, DateTimeKind.Utc).AddTicks(174));
+
+            migrationBuilder.UpdateData(
+                table: "Orders",
+                keyColumn: "OrderId",
+                keyValue: new Guid("86d3a045-b42d-4854-8150-d6a374948b6e"),
+                column: "OrderPlaced",
+                value: new DateTime(2024, 5, 22, 14, 50, 40, 369, DateTimeKind.Utc).AddTicks(186));
+
+            migrationBuilder.UpdateData(
+                table: "Orders",
+                keyColumn: "OrderId",
+                keyValue: new Guid("ba0eb0ef-b69b-46fd-b8e2-41b4178ae7cb"),
+                column: "OrderPlaced",
+                value: new DateTime(2024, 5, 22, 14, 50, 40, 369, DateTimeKind.Utc).AddTicks(238));
+
+            migrationBuilder.UpdateData(
+                table: "Orders",
+                keyColumn: "OrderId",
+                keyValue: new Guid("e6a2679c-79a3-4ef1-a478-6f4c91b405b6"),
+                column: "OrderPlaced",
+                value: new DateTime(2024, 5, 22, 14, 50, 40, 369, DateTimeKind.Utc).AddTicks(217));
+
+            migrationBuilder.UpdateData(
+                table: "Orders",
+                keyColumn: "OrderId",
+                keyValue: new Guid("f5a6a3a0-4227-4973-abb5-a63fbe725923"),
+                column: "OrderPlaced",
+                value: new DateTime(2024, 5, 22, 14, 50, 40, 369, DateTimeKind.Utc).AddTicks(228));
+        }
+    }
+}
