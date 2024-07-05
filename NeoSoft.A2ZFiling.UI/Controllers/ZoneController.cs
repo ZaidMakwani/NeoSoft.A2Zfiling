1d053c9bfa3292501958c81b0709ea2f62f87e79
saifaneosoft <saifa.ansari@neosoftmail.com>
2024-07-05 12:54:42 +0530

    some UI changes from development-saifa

--- NeoSoft.A2ZFiling.UI/Controllers/ZoneController.cs
+++ NeoSoft.A2ZFiling.UI/Controllers/ZoneController.cs
@@ -5,7 +5,7 @@ using NeoSoft.A2ZFiling.UI.ViewModels;
 
 namespace NeoSoft.A2ZFiling.UI.Controllers
 {
-    [CustomAuthorize]
+    //[CustomAuthorize]
     public class ZoneController : Controller
     {
         private readonly ILogger<ZoneController> _logger;
